b4a4c54e9ed62488de96379541a7d086b3333b81
lifq <lifuqiang@xinfushe.com>
2019-10-15 12:21:43 +0800

    feign

--- spring-cloud-consumer-fegin/src/main/java/com/example/springcloudconsumerfegin/service/IFeginService.java
+++ spring-cloud-consumer-fegin/src/main/java/com/example/springcloudconsumerfegin/service/IFeginService.java
@@ -2,11 +2,12 @@ package com.example.springcloudconsumerfegin.service;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //表示"user-service"的服务 提供
 @FeignClient(value = "user-service")
 public interface IFeginService {
  
     @RequestMapping(value = "/provider/getUser")
-    public String getUser(Integer id);
+    public String getUser(@RequestParam("id") Integer id);
 }
